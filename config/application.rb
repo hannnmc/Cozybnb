e75fb47b208fda2c7518dafd5cb60b714cd6c99c
Han Chen <hanmikechen@gmail.com>
2022-10-07 15:40:30 -0400

    add dependencies and enabling session

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,16 @@ module Cozybnb
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    module Backend
+      class Application < Rails::Application
+        # ...
+        config.middleware.use ActionDispatch::Cookies
+        config.middleware.use ActionDispatch::Session::CookieStore,
+          key: '_auth_me_session',
+          same_site: :lax, 
+          secure: Rails.env.production?
+      end
+    end
   end
 end
