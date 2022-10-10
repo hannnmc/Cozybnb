323fe64d14dedeacef56f4f714810ed5b2d7829a
Han Chen <hanmikechen@gmail.com>
2022-10-10 16:53:09 -0400

    add navbar styling

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import LoginForm from './LoginForm';
+import profileImg from '../../assets/images/profile.png'
+
 
 function LoginFormModal() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <button onClick={() => setShowModal(true)}>Log In</button>
+      <button className='profile-button' onClick={() => setShowModal(true)}>
+        <svg className='profile-burger' width={15} height={15} viewBox="0 0 32 32" fill={'black'} stroke={'#222222'} strokeWidth={3}>
+          <g fill="none" fill-rule="nonzero"><path d="m2 16h28"></path><path d="m2 24h28"></path><path d="m2 8h28"></path></g>
+        </svg>
+        <svg className='profile-icon' width={28} height={28} viewBox="0 0 32 32" fill={'#717171'}>
+          <path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path>
+        </svg>
+      </button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <LoginForm />
