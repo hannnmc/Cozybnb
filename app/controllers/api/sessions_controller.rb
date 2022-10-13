cf2c5768dd79231ec1f0916616b8e803d68eb356
Han Chen <hanmikechen@gmail.com>
2022-10-13 11:04:14 -0400

    fix errors on signup

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -9,7 +9,7 @@ class Api::SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_credentials(params[:credential], params[:password])
+    @user = User.find_by_credentials(params[:email], params[:password])
 
     if @user
       login!(@user)
