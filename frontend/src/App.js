8597c6e0fe1df89806bec4dcb5a406c44e94ede6
Han Chen <hanmikechen@gmail.com>
2022-10-14 00:39:51 -0400

    figuring aws fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+import { React, useState, useEffect } from 'react';
 import { Redirect, Route, Switch } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import ProfilePage from "./components/ProfilePage";
 
 function App() {
+
+  const initialState = { 
+    user: JSON.parse(sessionStorage.getItem("currentUser"))
+  };
+  const {user} = initialState
+
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await fetch(`/api/user/${user.id}`);
+      console.log('sdf');
+      setCurrentUser(await res.json());
+    }
+    fetchUser();
+  }, []);
+
   return (
     <>
       <Navigation />
         <Switch>
           <Route path="/profile">
-            <ProfilePage />
+            <ProfilePage user={user}/>
+            {/* <ProfilePage /> */}
           </Route>
           <Route path="/"></Route>
         </Switch>
