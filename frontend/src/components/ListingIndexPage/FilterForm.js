055c1afee55d94bc3f463582f460a407fa6d806d
Han Chen <hanmikechen@gmail.com>
2022-10-17 03:14:07 -0400

    style filters

--- frontend/src/components/ListingIndexPage/FilterForm.js
+++ frontend/src/components/ListingIndexPage/FilterForm.js
@@ -1,63 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Input } from '../Forms';
 import './FilterForm.css';
 
-function FilterForm({ minGuests, maxGuests, setMinGuests, setMaxGuests }) {
-  const parseValue = val => val === '' ? val : parseInt(val);
+function FilterForm({ minGuests, maxGuests, minPrice, maxPrice, setMinGuests, setMaxGuests, setMinPrice, setMaxPrice }) {
+    const parseValue = val => val === '' ? val : parseInt(val);   
+    const [ showFilter, setShowFilter ] = useState(false)
+
+    const toggleFilter = (e) => {
+        e.preventDefault();
+        setShowFilter(open => !open);
+        console.log(showFilter)
+    }
+    
 
   return (
     <div className="filter-form">
       <div className="filter-fields">
 
-        <div class="wrapper">
-            <Input
-            label="Minimum Guests:"
-            type="number"
-            value={minGuests}
-            onChange={e => setMinGuests(parseValue(e.target.value))}
-            />
-            <br />
-            <Input
-            label="Maximum Guests:"
-            type="number"
-            value={maxGuests}
-            onChange={e => setMaxGuests(parseValue(e.target.value))}
-            />
-      <header>
-
-      </header>
-      <div class="price-input">
-        <div class="field">
-          <span>Min</span>
-          <input type="number" class="input-min" value="1"/>
-        </div>
-        <div class="separator">-</div>
-        <div class="field">
-          <span>Max</span>
-          <input type="number" class="input-max" value={maxGuests}/>
-        </div>
-      </div>
-      <div class="slider">
-        <div class="progress"></div>
-      </div>
-      <div class="range-input">
-        <input type="range" class="range-min" min="100" max="2900" value={minGuests} step="30"/>
-        <input type="range" class="range-max" min="101" max="3000" value={maxGuests} step="30"/>
-      </div>
-        </div>
-
-
-
-      <div className='filter-button'>
-        <button>
-            <div>
-                <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" width={'16px'} height={'16px'} fill={'#222222'}><path d="M5 8c1.306 0 2.418.835 2.83 2H14v2H7.829A3.001 3.001 0 1 1 5 8zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm6-8a3 3 0 1 1-2.829 4H2V4h6.17A3.001 3.001 0 0 1 11 2zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"></path></svg>
+        <div className="wrapper">
+            <div className='filter-top'>
+                {toggleFilter && (
+                <div className="price-input">
+                <div className="field">
+                <Input
+                label="Min:"
+                type="number"
+                value={minPrice}
+                onChange={e => setMinPrice(parseValue(e.target.value))}
+                />
+                </div>
+                <div className="field">
+                <Input
+                label="Max:"
+                type="number"
+                value={maxPrice}
+                onChange={e => setMaxPrice(parseValue(e.target.value))}
+                />
+                </div>
+                </div>
+                )}
+                <div className='filter-button' onClick={toggleFilter}>
+                    <button>
+                        <div>
+                            <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" width={'16px'} height={'16px'} fill={'#222222'}><path d="M5 8c1.306 0 2.418.835 2.83 2H14v2H7.829A3.001 3.001 0 1 1 5 8zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm6-8a3 3 0 1 1-2.829 4H2V4h6.17A3.001 3.001 0 0 1 11 2zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"></path></svg>
+                        </div>
+                        <div>
+                            <span>Filters</span>
+                        </div>
+                    </button>
+                </div>
             </div>
-            <div>
-                <span>Filters</span>
+
+            <div className="slider">
+                <div className="progress"></div>
             </div>
-        </button>
-      </div>
+            <div className="range-input">
+                <input type="range" className="range-min" min="100" max="2900" value={minPrice} step="any"
+                onChange={e => setMinPrice(parseValue(e.target.value))}
+                />
+                <input type="range" className="range-max" min="101" max="3000" value={maxPrice} step="any" 
+                onChange={e => setMaxPrice(parseValue(e.target.value))}
+                />
+            </div>
+        </div>
+
+
+
       </div>
     </div>
   )
