95bff75d85a108b730b88316c9b350207be0f38d
Han Chen <hanmikechen@gmail.com>
2022-10-17 17:35:23 -0400

    unable to fix google maps api key

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,33 +3,11 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import ProfilePage from "./components/ProfilePage";
 import ListingIndexPage from './components/ListingIndexPage';
-// import csrfFetch from './store/csrf';
+import ListingShowPage from './components/ListingShowPage/ListingShowPage';
 import { useSelector } from 'react-redux';
 function App() {
 
-  // const initialState = { 
-  //   user: JSON.parse(sessionStorage.getItem("currentUser"))
-  // };
-  // const {user} = initialState
 
-  // const [currentUser, setCurrentUser] = useState(user);
-
-  // useEffect(() => {
-  //   const fetchUser = async () => {
-  //     const res = await csrfFetch(`/api/user/${user.id}`, {
-  //       headers : { 
-  //         'Content-Type': 'application/json',
-  //         'Accept': 'application/json'
-  //        }
-  //     });
-  //     // const data = await res.json();
-  //     console.log(user)
-  //     console.log(res)
-  //     console.log(res.json());
-  //     setCurrentUser(res.json());
-  //   }
-  //   fetchUser();
-  // }, []);
   const user = useSelector(({session}) => session.user);
   const loggedIn = !!user;
   return (
@@ -40,7 +18,10 @@ function App() {
             {loggedIn ? <ProfilePage/> : <Redirect to="/" />}
           </Route>
           <Route path="/">
-          <ListingIndexPage />
+            <ListingIndexPage />
+          </Route>
+          <Route path="/listings/:listingId">
+            <ListingShowPage />
           </Route>
         </Switch>
     </>
