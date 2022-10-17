4887d26d92ccb114cb99486d7981f28e66f07ae8
Han Chen <hanmikechen@gmail.com>
2022-10-17 00:04:11 -0400

    adjust filter bar

--- frontend/src/components/ListingIndexPage/index.js
+++ frontend/src/components/ListingIndexPage/index.js
@@ -13,12 +13,20 @@ function ListingIndexPage() {
   const dispatch = useDispatch();
   const listings = useSelector(state => Object.values(state.listings));
   const [minGuests, setMinGuests] = useState(1);
-  const [maxGuests, setMaxGuests] = useState(10);
+  const [maxGuests, setMaxGuests] = useState(16);
+  const [minPrice, setMinPrice] = useState(100);
+  const [maxPrice, setMaxPrice] = useState(3000);
   const [highlightedListing, setHighlightedListing] = useState(null);
   const [bounds, setBounds] = useState(null);
 
   useEffect(() => {
     if (minGuests && maxGuests && bounds) {
+      dispatch(fetchListings({ minGuests, maxGuests, bounds }));
+    }
+  }, [minGuests, maxGuests, bounds, dispatch]);
+
+  useEffect(() => {
+    if (minPrice && maxPrice && bounds) {
       dispatch(fetchListings({ minGuests, maxGuests, bounds }));
     }
   }, [minGuests, maxGuests, bounds, dispatch]);
