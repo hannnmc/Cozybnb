a0987ae3c2711aeff4b5dd5031b4ab2e7d103b0b
Han Chen <hanmikechen@gmail.com>
2022-10-18 16:36:06 -0400

    dynamic host name done

--- frontend/src/store/users.js
+++ frontend/src/store/users.js
@@ -1,11 +1,17 @@
 import csrfFetch from "./csrf";
 
 const SET_USERS = 'users/setUsers'
+const SET_USER = 'users/setUser'
 
 export const setUsers = (users) => ({
     type: SET_USERS,
     payload: users
 });
+
+export const setUser = (user) => ({
+    type: SET_USER,
+    payload: user
+})
 
 export const fetchUsers = () => async dispatch => {
     const res = await csrfFetch('/api/users')
@@ -14,15 +20,27 @@ export const fetchUsers = () => async dispatch => {
         dispatch(setUsers(data.users));
     }
 }
+
+// export const fetchUser = (userId) => async dispatch => {
+//     const res = await csrfFetch(`/api/users/${userId}`)
+//     if (res.ok) {
+//         const data = await res.json();
+//         dispatch(setUser(data.user));
+//     }
+//     return data.user
+// }
+
  
 function usersReducer(state = {}, action) {
     Object.freeze(state);
     const nextState = {...state};
     switch (action.type) {
-      case SET_USERS:
-        return {...nextState,...action.payload};
-      default:
-        return state;
+        case SET_USER:
+
+        case SET_USERS:
+            return {...nextState,...action.payload};
+        default:
+            return state;
     }
 }
 
