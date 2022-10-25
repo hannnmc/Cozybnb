b6ec684336d8693ca5ccdd769a761d771ba91e2e
Han Chen <hanmikechen@gmail.com>
2022-10-24 21:22:17 -0400

    add code snippet

--- frontend/src/components/ListingIndexPage/index.js
+++ frontend/src/components/ListingIndexPage/index.js
@@ -64,10 +64,6 @@ function ListingIndexPage() {
       </div>
       <div className="listing-list-container">
         <FilterForm
-          minGuests={minGuests}
-          maxGuests={maxGuests}
-          setMinGuests={setMinGuests}
-          setMaxGuests={setMaxGuests}
           minPrice={minPrice}
           maxPrice={maxPrice}
           setMinPrice={setMinPrice}
