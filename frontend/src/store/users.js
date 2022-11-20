605461d133e6ffe36d741dc63875cca63a04d51e
Han Chen <hanmikechen@gmail.com>
2022-11-19 20:03:55 -0500

    got reviews to front end state

--- frontend/src/store/users.js
+++ frontend/src/store/users.js
@@ -1,25 +1,25 @@
 import csrfFetch from "./csrf";
 import * as sessionActions from "./session";
 
-const SET_USERS = 'users/setUsers';
-const SET_USER = 'users/setUser';
+const ADD_USERS = 'users/addUsers';
+const ADD_USER = 'users/addUser';
 
 
-export const setUsers = (users) => ({
-    type: SET_USERS,
-    payload: users
-});
-
-export const setUser = (user) => ({
-    type: SET_USER,
+export const addUser = (user) => ({
+    type: ADD_USER,
     payload: user
-});
+  });
+  
+  export const addUsers = (users) => ({
+    type: ADD_USERS,
+    payload: users
+  });
 
 export const fetchUsers = () => async dispatch => {
     const res = await csrfFetch('/api/users')
     if (res.ok) {
         const data = await res.json();
-        dispatch(setUsers(data.users));
+        dispatch(addUsers(data.users));
     }
 };
 
@@ -60,10 +60,10 @@ function usersReducer(state = {}, action) {
     Object.freeze(state);
     const nextState = {...state};
     switch (action.type) {
-        case SET_USER:
+        case ADD_USER:
             nextState[action.payload.id] = action.payload;
             return nextState;
-        case SET_USERS:
+        case ADD_USERS:
             return {...nextState,...action.payload};
         default:
             return state;
