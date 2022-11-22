a578d6ddc8699e7cb97508ebe722928ee130fde9
Han Chen <hanmikechen@gmail.com>
2022-11-21 23:57:41 -0500

    style listing item review decimal points and filter style

--- frontend/src/components/ListingIndexPage/ListItem.js
+++ frontend/src/components/ListingIndexPage/ListItem.js
@@ -36,7 +36,7 @@ function ListItem({ listing, isHighlighted, setSelectedListing }) {
           <h2  className="list-item-title">{title}</h2>
           <span>
           <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" height='12px' width='12px' ><path d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z" fillRule="evenodd"></path></svg>
-            {` ${avgListingReview > 0 ? avgListingReview : 'New'}`}</span>
+            {` ${avgListingReview > 0 ? parseFloat(avgListingReview).toFixed(1) : 'New'}`}</span>
         </div>
         <div className="list-item-fields">
           <div className="top-info-field">
