83be3a9edb95afba3709b0cc6fe14f4970cb977b
Han Chen <hanmikechen@gmail.com>
2022-11-23 02:12:49 -0500

    update profile visuals

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -17,6 +17,10 @@ const ProfilePage = () => {
     const reviews = useSelector(state => Object.values(state.reviews));
     const users = useSelector(state => state.users);
 
+    const uploadPhoto = (e) => {
+        e.preventDefault();
+    }
+
     const openEditProfile = (e) => {
         e.preventDefault();
         setShowProfileEditForm(open => !open);
@@ -63,16 +67,27 @@ const ProfilePage = () => {
             <div className='profile-card'>
                 <div className='profile-card-padding'>
                 <div className='profile-card-header'>
-                    <img className='profile-picture' src={user.photoUrl} alt="profile" />
+                    {user.photoUrl && (<img className='profile-picture' src={user.photoUrl} alt="loading" />)}
                 </div>
-                {/* <div>Update photo</div> */}
+                <label className='update-photo' htmlFor="update-photo">Update photo</label>
+                <input type="file" id='update-photo'/>
+
                 <div className='identity-box'>
-                    <div className='badge-icon-div'>
-                        <svg className='badge-icon' width={24} height={24} viewBox="0 0 32 32" >
-                            <path d="M16 .798l.555.37C20.398 3.73 24.208 5 28 5h1v12.5C29 25.574 23.21 31 16 31S3 25.574 3 17.5V5h1c3.792 0 7.602-1.27 11.445-3.832L16 .798zm0 2.394l-.337.213C12.245 5.52 8.805 6.706 5.352 6.952L5 6.972V17.5c0 6.831 4.716 11.357 10.713 11.497L16 29c6.133 0 11-4.56 11-11.5V6.972l-.352-.02c-3.453-.246-6.893-1.432-10.311-3.547L16 3.192zm7 7.394L24.414 12 13.5 22.914 7.586 17 9 15.586l4.5 4.499 9.5-9.5z"></path>
-                        </svg>
+
+                    <div className='badge-box'>
+                        <div className='badge-icon-div'>
+                            <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" height='24px' width='24px'><path d="M14.998 1.032a2 2 0 0 0-.815.89l-3.606 7.766L1.951 10.8a2 2 0 0 0-1.728 2.24l.031.175A2 2 0 0 0 .87 14.27l6.36 5.726-1.716 8.608a2 2 0 0 0 1.57 2.352l.18.028a2 2 0 0 0 1.215-.259l7.519-4.358 7.52 4.358a2 2 0 0 0 2.734-.727l.084-.162a2 2 0 0 0 .147-1.232l-1.717-8.608 6.361-5.726a2 2 0 0 0 .148-2.825l-.125-.127a2 2 0 0 0-1.105-.518l-8.627-1.113-3.606-7.765a2 2 0 0 0-2.656-.971zm-3.07 10.499l4.07-8.766 4.07 8.766 9.72 1.252-7.206 6.489 1.938 9.723-8.523-4.94-8.522 4.94 1.939-9.723-7.207-6.489z"></path></svg>
+                        </div>
+                        <div className='identity-verified'>{ownedReservations.length} reviews</div>
                     </div>
-                    <div className='identity-verified'>Identity verified</div>
+                    <div className='badge-box'>
+                        <div className='badge-icon-div'>
+                            <svg className='badge-icon' width={24} height={24} viewBox="0 0 32 32" >
+                                <path d="M16 .798l.555.37C20.398 3.73 24.208 5 28 5h1v12.5C29 25.574 23.21 31 16 31S3 25.574 3 17.5V5h1c3.792 0 7.602-1.27 11.445-3.832L16 .798zm0 2.394l-.337.213C12.245 5.52 8.805 6.706 5.352 6.952L5 6.972V17.5c0 6.831 4.716 11.357 10.713 11.497L16 29c6.133 0 11-4.56 11-11.5V6.972l-.352-.02c-3.453-.246-6.893-1.432-10.311-3.547L16 3.192zm7 7.394L24.414 12 13.5 22.914 7.586 17 9 15.586l4.5 4.499 9.5-9.5z"></path>
+                            </svg>
+                        </div>
+                        <div className='identity-verified'>Identity verified</div>
+                    </div>
                 </div>
                 {/* <div>
                     Show others you're really you with the identity verification badge.
@@ -94,6 +109,7 @@ const ProfilePage = () => {
                     <div>Email address</div>
                 </div>
                 {/* <div className='profile-box-divider'></div> */}
+                <div className='profile-box-subscript'><a href="https://github.com/hannnmc/Cozybnb" target="_blank" rel="noopener noreferrer">Learn more</a> about how confirming account info helps keep the Cozybnb community secure.</div>
             
                 </div>
                 </div>
@@ -145,7 +161,7 @@ const ProfilePage = () => {
                 <div id='profile-box-divider' className='profile-box-divider'></div>
                 <div className='review-count-container'>
                     <div className='profile-review-header'>
-                        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" height='24px' width='24px'><path d="M14.998 1.032a2 2 0 0 0-.815.89l-3.606 7.766L1.951 10.8a2 2 0 0 0-1.728 2.24l.031.175A2 2 0 0 0 .87 14.27l6.36 5.726-1.716 8.608a2 2 0 0 0 1.57 2.352l.18.028a2 2 0 0 0 1.215-.259l7.519-4.358 7.52 4.358a2 2 0 0 0 2.734-.727l.084-.162a2 2 0 0 0 .147-1.232l-1.717-8.608 6.361-5.726a2 2 0 0 0 .148-2.825l-.125-.127a2 2 0 0 0-1.105-.518l-8.627-1.113-3.606-7.765a2 2 0 0 0-2.656-.971zm-3.07 10.499l4.07-8.766 4.07 8.766 9.72 1.252-7.206 6.489 1.938 9.723-8.523-4.94-8.522 4.94 1.939-9.723-7.207-6.489z"></path></svg>
+                        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" height='16px' width='16px'><path d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z" fill-rule="evenodd"></path></svg>
                         <div className='user-review-count'>{ownedReviews.length} reviews</div>
                     </div>
                     <ProfileReviews 
