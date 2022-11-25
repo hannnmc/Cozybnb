22ced17f4df7f1e497b91aa82f02fe84236a200e
Han Chen <hanmikechen@gmail.com>
2022-11-25 17:40:07 -0500

    render edit button on owned listings

--- frontend/src/components/ProfileListingItem/index.js
+++ frontend/src/components/ProfileListingItem/index.js
@@ -9,7 +9,7 @@ function ProfileListingItem({ ownedListings, listing }) {
     const { title, photoUrls, price, averageRating, description, beds } = listing;
     const dispatch = useDispatch();
 
-    const handleDelete = (e) =>{
+    const handleEdit = (e) =>{
         e.preventDefault();
         e.stopPropagation();
 
@@ -19,41 +19,35 @@ function ProfileListingItem({ ownedListings, listing }) {
         <div
         onClick={() => history.push(`/listings/${listing.id}`)}
         >
-        <div className="profile-listing-info">
-        {photoUrls && ( <div className="profile-image-div"> <img className="profile-listing-image" src={photoUrls[0]} alt='loading...'/></div> )}
-        <div 
-            onClick={handleDelete}
-            className='delete-reservation'>
-            <i className="fa-solid fa-x"></i>
-        </div> 
-        <div className="profile-listing-infobox">
-            <h2  className="profile-item-title">{title}</h2>
-            <div className="profile-listing-fields">
-                <div className="profile-listing-info-top">
-                    {/* <span className="list-item-category">Average Rating:</span> */}
-                    {/* <span className="list-item-review">
-                    {averageRating || ' No reviews yet'}
-                    </span> */}
-                    <div>
-                        <span>{description.split(' ').slice(0,5).join(' ')}</span>
-                        <span className="description-dots">...</span>
+            <div className="profile-listing-info">
+            {photoUrls && ( <div className="profile-image-div"> <img className="profile-listing-image" src={photoUrls[0]} alt='loading...'/></div> )}
+                <div className="profile-listing-infobox">
+                    <h2  className="profile-item-title">{title}</h2>
+                    <div className="profile-listing-fields">
+                        <div className="profile-listing-info-top">
+                            <div>
+                                <span>{description.split(' ').slice(0,5).join(' ')}</span>
+                                <span className="description-dots">...</span>
 
+                            </div>
+                            <span className="list-item-beds">
+                                {beds} beds
+                            </span>
+                        </div>
+                        <div className="profile-listing-info-top">
+                            <div>
+                                <span className="list-item-copy">${price}</span>
+                                <span className="list-item-category"> night</span>
+                            </div>
+                        </div>
                     </div>
-                    <span className="list-item-beds">
-                        {beds} beds
-                    </span>
                 </div>
-                <div className="profile-listing-info-top">
-                    <div>
-                        <span className="list-item-copy">${price}</span>
-                        <span className="list-item-category"> night</span>
-                    </div>
-                </div>
+                <div 
+                    onClick={handleEdit}
+                    className='update-listing'>
+                    <i class="fa-regular fa-pen-to-square"></i>
+                </div> 
             </div>
-        </div>
-
-            
-        </div>
         </div>
     );
 }
