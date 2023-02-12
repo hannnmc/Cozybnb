f7ed14e81210355ddacf825869d2ccafc9caabee
Han Chen <hanmikechen@gmail.com>
2023-02-12 17:36:37 -0500

    style searchbar

--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -1,10 +1,14 @@
 import './Search.css';
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
-import { useEffect, useState } from 'react';
+import GooglePlacesAutocomplete, { geocodeByLatLng } from 'react-google-places-autocomplete';
+import React, { useEffect, useState } from 'react';
 
 const Search = (props) => {
-
+    
     const [value, setValue] = useState(null);
+
+    useEffect(() => {
+        console.log(value);
+    },[value])
 
     // useEffect((e) => {
     //     console.log(value);
@@ -15,12 +19,6 @@ const Search = (props) => {
             <GooglePlacesAutocomplete
             apiKey={process.env.REACT_APP_MAPS_API_KEY}
             selectProps={{
-                style: {
-                    option: (provided) => ({
-                        ...provided,
-                        color: 'red',
-                    })
-                },
                 value,
                 onchange: setValue,
                 placeholder: "Search...",
@@ -28,7 +26,7 @@ const Search = (props) => {
             }}
             />
             <div className='search-button'>
-                <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" height="12px" width="12px" display="block" stroke="currentcolor" stroke-width="5.33333" overflow="visible"><g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg>
+                <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" height="12px" width="12px" display="block" stroke="currentcolor" strokeWidth="5.33333" overflow="visible"><g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg>
             </div>
         </div>
     );
